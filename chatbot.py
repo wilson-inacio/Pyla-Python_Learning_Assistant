59fe03f97d1ba40166fe16e7bab3137e5e14e6a4
Wilson Inacio <227900222+wilson-inacio@users.noreply.github.com>
2026-02-12 11:20:06 +0000

    updated readme

--- chatbot.py
+++ chatbot.py
@@ -7,8 +7,6 @@ Created on Tue Feb 10 22:26:09 2026
 # Importing relevant libraries
 import json
 import random
-import numpy as np
-from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
@@ -25,37 +23,11 @@ class ChatBot:
 
         self.intents = data["intents"]
         self.responses = data["responses"]
-    
-        # Train word2vec
-        self.train_word2vec()
         
         # Build TF-IDF + Logistic Regression model
         self.vectorizer, self.model = self.vec_model(self.intents)
-       
-    def suggest_related_terms(self, word):
-        if word in self.w2v_model.wv:
-            similar = self.w2v_model.wv.most_similar(word, topn=3)
-            return [w for w, _ in similar]
-        return []
     
-    # word2vec model
-    def train_word2vec(self):
-        sentences = []
-    
-        for examples in self.intents.values():
-            for sentence in examples:
-                tokens = sentence.lower().split()
-                sentences.append(tokens)
-    
-        self.w2v_model = Word2Vec(
-            sentences,
-            vector_size=100,
-            window=3,
-            min_count=1,
-            workers=4
-        )
-    
-    # Apply word2vec
+    # Apply vectorizer and Logistic regression model
     def vec_model(self, intents):
         sentences = []
         labels = []
@@ -140,6 +112,7 @@ class ChatBot:
         
         # Keep conversation going
         while not self.end_chat(reply):
+        # Vectorize input and predict intent
             reply_vec = self.vectorizer.transform([reply])
             intent = self.model.predict(reply_vec)[0]
                 
@@ -156,14 +129,7 @@ class ChatBot:
             # Extract entities
             entities = self.extract_entities(reply)
             
-            # Suggest related terms
-            for entity_word, _ in entities:
-                related = self.suggest_related_terms(entity_word)
-                if related:
-                    print(f"Pyla: Related concepts: {', '.join(related)}")
-
-            
-            # Get safe response
+            # Get response
             bot_reply = self.get_response(intent, entities)
             reply = input(f"Pyla: {bot_reply}").lower()
         
